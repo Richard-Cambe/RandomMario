e90800b8bf795832b150f7a3129bbd2c40600695
Richard-Cambe <richard.cambe@outlook.fr>
2025-02-03 23:27:35 +0100

    sound working but delayed...

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 import {Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useState, useEffect, useRef} from 'react';
 import {data} from './allMaps'
+import {useFonts} from "expo-font";
+import { Audio } from 'expo-av';
 
 export default function App() {
     const [map, setMap] = useState(null);
     const [animationFinished, setAnimationFinished] = useState(false);
     const [imageIconIndex, setImageIconIndex] = useState(0);
+    const [sound, setSound] = useState();
 
     const imageIconLength = data.length;
     const interval = useRef(null)
 
+    const [fontsLoaded] = useFonts({
+        'ShinGoProRegular': require('../assets/fonts/AOTFShinGoProRegular.otf'),
+        'ShinGoProBold': require('../assets/fonts/AOTFShinGoProBold.otf'),
+        'ShinGoProDeBold': require('../assets/fonts/AOTFShinGoProDeBold.otf'),
+        'ShinGoProHeavy': require('../assets/fonts/AOTFShinGoProHeavy.otf'),
+        'ShinGoProExLight': require('../assets/fonts/AOTFShinGoProExLight.otf'),
+        'ShinGoProLight': require('../assets/fonts/AOTFShinGoProLight.otf'),
+        'ShinGoProMedium': require('../assets/fonts/AOTFShinGoProMedium.otf'),
+        'SuperMario256': require('../assets/fonts/SuperMario256.ttf'),
+    });
+
+    const loadSound = async () => {
+        const { sound } = await Audio.Sound.createAsync(
+            require('../assets/sounds/Item Box.mp3'),
+        );
+        setSound(sound);
+    };
+
+    const playSound = async () => {
+        if (sound) {
+            await sound.replayAsync()
+        }
+    }
+
     const chosenMap = () => {
-        const randomMapId =  Math.floor(Math.random() * imageIconLength);
+        const randomMapId = Math.floor(Math.random() * imageIconLength);
         const mapData = data[randomMapId];
 
         setAnimationFinished(false);
@@ -19,7 +46,8 @@ export default function App() {
         let animationTimer = 0
 
         interval.current = setInterval(() => {
-            setImageIconIndex((prevIndex) => (prevIndex +1) % imageIconLength);
+            setImageIconIndex((prevIndex) => (prevIndex + 1) % imageIconLength);
+            playSound();
             animationTimer += 100;
             if (animationTimer >= 4000) {
                 clearInterval(interval.current);
@@ -30,7 +58,12 @@ export default function App() {
     };
 
     useEffect(() => {
+        loadSound();
+
         return () => clearInterval(interval.current);
+        if (sound) {
+            sound.unloadAsync();
+        }
     }, []);
 
     return (
@@ -40,17 +73,18 @@ export default function App() {
                                   style={styles.TouchableButton}>
                     <Text style={styles.TouchableText}>Choisir une carte</Text>
                 </TouchableOpacity>
-                { !animationFinished ? (
+                {!animationFinished ? (
                     <View>
-                        <Image style={styles.Icon} source={data[imageIconIndex].boardIcon} />
+                        <Image style={styles.Icon} source={data[imageIconIndex].boardIcon}/>
                     </View>
-                ) : (
-                    <Image style={styles.Icon} source={map.boardIcon}/>
+                ) : (<View>
+                        <Text style={styles.NameText}>{map.name}</Text>
+                        <Image style={styles.Icon} source={map.boardIcon}/>
+                    </View>
                 )}
-
             </View>
         </>
-);
+    );
 }
 
 
@@ -69,12 +103,10 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     TouchableText: {
+        fontFamily: 'ShinGoProExtraBold',
         color: '#fff',
         fontSize: 18,
         fontWeight: 'bold',
-    },
-    noText: {
-        color:'red',
     },
     NameText: {
         fontSize: 20,
@@ -82,7 +114,7 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
     },
-    Icon:{
-        marginVertical:30,
-    }
+    Icon: {
+        marginVertical: 30,
+    },
 });
