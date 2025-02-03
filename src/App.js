571a8aced4bf1f63a3761aaaedb485be2fdb8c8f
Richard-Cambe <richard.cambe@outlook.fr>
2025-02-03 19:06:23 +0100

    fixed randomizer

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import {Button, Image, StyleSheet, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import React, {useState} from 'react';
 import {data} from './allMaps'
 
 export default function App() {
 
-    const [index, setIndex] = useState(0);
+    const [map, setMap] = useState(null);
 
-    let chosenMap = (min, max) => {
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return (
-        <>
-            <View style={styles.container}>
-                <Image
-                    source ={require("../assets/board-icons/super-mario-party-jamboree-board-icon-2.png")}
-                    style={{width: 200, height: 200}}
-                    resizeMode={"contain"}
-                />
-                <Button onPress={() => setIndex(chosenMap())}
-                        title="Choisir une carte"
-                        color={"orange"}
-                />
-            </View>
-        </>
-    );
-}
+    const chosenMap = () => {
+        const randomMapId =  Math.floor(Math.random() * 7);
+        const mapIcon = data[randomMapId];
+        setMap(map);
+    };
+
+//     return (
+//         <>
+//             <View style={styles.container}>
+//                 <Image
+//                     source={require("../assets/board-icons/super-mario-party-jamboree-board-icon-2.png")}
+//                     style={{width: 200, height: 200}}
+//                     resizeMode={"contain"}
+//                 />
+//                 <TouchableOpacity onPress={() => setIndex(chosenMap(1, 7))}
+//                                   style={styles.TouchableButton}>/>
+//                     <Text style={styles.TouchableText}>Choisir une carte</Text>
+//                 </TouchableOpacity>
+//                     <Text>{index}</Text>
+//             </View>
+//         </>
+// );
+// }
 
 const styles = StyleSheet.create({
     container: {
@@ -32,5 +36,17 @@ const styles = StyleSheet.create({
         backgroundColor: 'red',
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    TouchableButton: {
+        backgroundColor: 'orange',
+        padding: 10,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    TouchableText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
     },
 });
