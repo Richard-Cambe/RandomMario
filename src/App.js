29089f5164e4a4fd4622cf9800d12cf53e577260
Richard-Cambe <richard.cambe@outlook.fr>
2025-02-03 16:57:55 +0100

    fixed image (not dynamic)

--- src/App.js
+++ src/App.js
@@ -1,30 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import {Button, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
-import React, { useState } from 'react';
-import { data } from './allMaps'
+import {Button, Image, StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {data} from './allMaps'
 
 export default function App() {
 
     const [index, setIndex] = useState(0);
 
     let chosenMap = (min, max) => {
-        return Math.floor(Math.random() * (1-7));
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-  return (
-      <>
-        <View style={styles.container}>
-            <Button onPress={() => setIndex(chosenMap())}
-                              title="Choisir une carte"
-            />
-      </View>
-      </>);
+    return (
+        <>
+            <View style={styles.container}>
+                <Image
+                    source ={require("../assets/board-icons/super-mario-party-jamboree-board-icon-2.png")}
+                    style={{width: 200, height: 200}}
+                    resizeMode={"contain"}
+                />
+                <Button onPress={() => setIndex(chosenMap())}
+                        title="Choisir une carte"
+                        color={"orange"}
+                />
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'red',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
