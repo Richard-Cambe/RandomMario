cfc2c0e327a5346d403092a55a78cc3a18d07629
Richard-Cambe <richard.cambe@outlook.fr>
2025-02-08 19:11:52 +0100

    WIP -- buggued store (translateY is the problem)

--- src/App.js
+++ src/App.js
@@ -1,96 +1,56 @@
-import {Image, StyleSheet, Text, TouchableOpacity, Vibration, View} from 'react-native';
-import React, {useState, useEffect, useRef} from 'react';
-import {data} from './allMaps'
-import {useFonts} from "expo-font";
-import {Audio} from 'expo-av';
-import Animated, { useSharedValue, withTiming, withRepeat, ReduceMotion } from "react-native-reanimated";
+import { Image, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useEffect, useRef } from 'react';
+import { data } from './allMaps';
+import { useFonts } from "expo-font";
+import { Audio } from 'expo-av';
+import Animated from "react-native-reanimated";
 import * as Haptics from 'expo-haptics';
+import { useStore } from "./store";
 
 export default function App() {
-    const [map, setMap] = useState(null);
-    const [animationFinished, setAnimationFinished] = useState(false);
-    const [imageIconIndex, setImageIconIndex] = useState(0);
-    const [sound, setSound] = useState();
-    const [sound2, setSound2] = useState();
-
-    const [buttonColor, setButtonColor] = useState("#00bfff");
-    const colors = ["#ff1493", "#9acd32", "#ffa500", "#00bfff"];
-    const changeColor = () => {
-        const randomColor = colors[Math.floor(Math.random() * colors.length)];
-        setButtonColor(randomColor);
-    };
-
+    const {
+        map, setMap,
+        animationFinished, setAnimationFinished,
+        imageIconIndex, setImageIconIndex,
+        sound, setSound,
+        sound2, setSound2,
+        buttonColor, changeColor,
+        translateY, startTranslateYAnimation
+    } = useStore();
 
     const imageIconLength = data.length;
-    const interval = useRef(null)
+    const interval = useRef(null);
 
     const [loaded, error] = useFonts({
         'SuperMario256': require('../assets/fonts/SuperMario256.ttf'),
     });
 
     const loadSound = async () => {
-        const {sound} = await Audio.Sound.createAsync(
+        const { sound } = await Audio.Sound.createAsync(
             require('../assets/sounds/Box.mp3')
         );
         setSound(sound);
     };
 
     const loadSound2 = async () => {
-        const {sound} = await Audio.Sound.createAsync(
+        const { sound } = await Audio.Sound.createAsync(
             require('../assets/sounds/randomize-mee-101soundboards.mp3')
         );
         setSound2(sound);
-    }
+    };
 
     const playSound = async () => {
         if (sound) {
-            await sound.stopAsync()
-            await sound.playAsync()
+            await sound.stopAsync();
+            await sound.playAsync();
         }
-    };
-
-    const playSound2 = async () => {
-        if (sound2) {
-            await sound2.stopAsync()
-            await sound2.playAsync()
-        }
-    };
-
-    const translateY = useSharedValue(-2);
-
-    const chosenMap = () => {
-        const randomMapId = Math.floor(Math.random() * imageIconLength);
-        const mapData = data[randomMapId];
-
-        setAnimationFinished(false);
-        setMap(null);
-        let animationTimer = 0
-        playSound();
-
-        interval.current = setInterval(() => {
-            setImageIconIndex((prevIndex) => (prevIndex + 1) % imageIconLength);
-            Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
-            animationTimer += 100;
-            if (animationTimer >= 6000) {
-                Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
-                clearInterval(interval.current);
-                setMap(mapData);
-                setAnimationFinished(true);
-            }
-        }, 50);
     };
 
     useEffect(() => {
         loadSound();
         loadSound2();
+        startTranslateYAnimation();
 
-        translateY.value = withRepeat(
-            withTiming(50, { duration: 800 }),
-            -1,
-            true,
-            () => {},
-            ReduceMotion.System,
-        )
         if (sound) {
             sound.unloadAsync();
         } else if (sound2) {
@@ -100,43 +60,47 @@ export default function App() {
         return () => clearInterval(interval.current);
     }, []);
 
-    return <>
+    return (
         <View style={styles.container}>
             <View style={styles.logoContainer}>
-                <Image style={styles.Logo} source={require('../assets/pics/Logo.png')}></Image>
+                <Image style={styles.Logo} source={require('../assets/pics/Logo.png')} />
             </View>
             <View style={styles.logoContainer}>
                 <TouchableOpacity onPress={() => {
-                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium)
-                    playSound2()}}>
-                    <Animated.Image style={[styles.Title,{transform: [{translateY}]}]}
-                                    source={require('../assets/pics/randomize-me-2-4-2025.png')}/>
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Medium);
+                    playSound2();
+                }}>
+                    <Animated.Image style={[styles.Title, { transform: [{ translateY }] }]} source={require('../assets/pics/randomize-me-2-4-2025.png')} />
                 </TouchableOpacity>
             </View>
 
             <View>
-                {!animationFinished ? <View>
-                    <Image style={styles.Icon} source={data[imageIconIndex].boardIcon}/>
-                </View> : <View>
-                    <Text style={styles.NameText}>{map.name}</Text>
-                    <Image style={styles.Icon} source={map.boardIcon}/>
-                </View>}
+                {!animationFinished ? (
+                    <View>
+                        <Image style={styles.Icon} source={data[imageIconIndex].boardIcon} />
+                    </View>
+                ) : (
+                    <View>
+                        <Text style={styles.NameText}>{map.name}</Text>
+                        <Image style={styles.Icon} source={map.boardIcon} />
+                    </View>
+                )}
             </View>
 
             <View style={styles.touchableContainer}>
                 <TouchableOpacity
                     onPress={() => {
-                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy)
+                        Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
                         chosenMap();
                         changeColor();
                     }}
-                    style={[styles.TouchableButton, {backgroundColor: buttonColor}]}>
-                    <Text style={styles.TouchableText}>Choisir une carte</Text>
+                    style={[styles.TouchableButton, { backgroundColor: buttonColor }]}
+                >
+                    <Text style={styles.TouchableText}>RANDOMIZE ME</Text>
                 </TouchableOpacity>
             </View>
         </View>
-    </>
-        ;
+    );
 }
 
 
@@ -195,5 +159,5 @@ const styles = StyleSheet.create({
     Icon: {
         position: 'relative',
         alignSelf: 'center',
-    }
+    },
 });
--- src/store.js
+++ src/store.js
@@ -1,7 +1,35 @@
-import { create } from 'zustand'
+import { create } from 'zustand';
+import { useSharedValue, withTiming, withRepeat, ReduceMotion } from 'react-native-reanimated';
 
-export const useStore = create((set) =>({
+const translatyY = useSharedValue(-2);
 
-    index: 0,
+export const useStore = create((set) => ({
+    map: null,
+    animationFinished: false,
+    imageIconIndex: 0,
+    sound: null,
+    sound2: null,
+    buttonColor: "#00bfff",
+    colors: ["#ff1493", "#9acd32", "#ffa500", "#00bfff"],
+    translateY,
 
-}))
+    setMap: (newMap) => set({ map: newMap }),
+    setAnimationFinished: (status) => set({ animationFinished: status }),
+    setImageIconIndex: (index) => set({ imageIconIndex: index }),
+    setSound: (newSound) => set({ sound: newSound }),
+    setSound2: (newSound) => set({ sound2: newSound }),
+    changeColor: () => set((state) => ({
+        buttonColor: state.colors[Math.floor(Math.random() * state.colors.length)]
+    })),
+    startTranslateYAnimation: () => {
+        set((state) => {
+            state.translateY.value = withRepeat(
+                withTiming(40, { duration: 1500 }),
+                -1,
+                true,
+                () => {},
+                ReduceMotion.System
+            );
+        });
+    }
+}));
